627957277dca3f54f8ff5090ec649dc8c886c96b
William <williamtao345@gmail.com>
2023-07-13 18:32:39 +0800

    Add creation date recognition for JPG files

--- file_name_changer.py
+++ file_name_changer.py
@@ -1,17 +1,56 @@
 import os
 import datetime
-
+import exifread
 
 date_format = "%Y-%m-%d %H.%M.%S"
 
 
-def get_modified_date(file_path):
-    file_info = os.stat(file_path)
+def reform_date_time_original(date_time_original):
+    split_date = str(date_time_original).split(" ")
 
-    file_stime = file_info.st_mtime
-    file_ctime = file_info.st_ctime
+    if len(split_date) != 2:
+        return None
+    else:
+        first_half = split_date[0].replace(":", "-")
+        second_half = split_date[1].replace(":", ".")
+        return first_half + " " + second_half
 
-    file_time = min(file_stime, file_ctime)
+
+def get_date_time_original(file_path_absolute):
+    with open(file_path_absolute, 'rb') as file_data:
+        try:
+            tags = exifread.process_file(file_data)
+        except Exception:
+            return None
+
+        tag_date = 'EXIF DateTimeOriginal'
+
+        if tag_date in tags:
+            return reform_date_time_original(str(tags[tag_date]))
+        else:
+            return None
+
+
+def get_time_of_modification(file_path_absolute):
+    file_info = os.stat(file_path_absolute)
+    return file_info.st_mtime
+
+
+def get_time_of_last_change(file_path_absolute):
+    file_info = os.stat(file_path_absolute)
+    return file_info.st_ctime
+
+
+def get_time_of_creation(file_path_absolute):
+    if get_file_type(file_path_absolute).lower() in ["jpg", "jpeg", "tiff"]:
+        date_time_original = get_date_time_original(file_path_absolute)
+        if date_time_original is not None:
+            return date_time_original
+
+    time_of_last_change = get_time_of_last_change(file_path_absolute)
+    time_of_last_modification = get_time_of_modification(file_path_absolute)
+
+    file_time = min(time_of_last_change, time_of_last_modification)
 
     file_datetime = datetime.datetime.fromtimestamp(file_time)
     return file_datetime.strftime(date_format)
@@ -34,14 +73,22 @@ def append_file_type(file_path, file_type):
     return file_path + "." + file_type
 
 
+def get_file_type(file_name):
+    if "." in current_file_name:
+        return str(file_name).split(".").pop()
+    else:
+        return ""
+
+
 path = os.getcwd()
 
+print("----------------------------------------------------------------------------------------")
 print("Instructions:")
 print("This program will ignores all the folders under the path., only change all the files.")
 print("You will be asked if you want to change each file.")
 print()
 print("Hope you enjoy! -- William Tao")
-print()
+print("----------------------------------------------------------------------------------------")
 print(f"Current folder path: {path}   Press enter to continue.", end="")
 input()
 print()
@@ -53,17 +100,16 @@ need_change_all_files = False
 for i in file:
     current_file_name = str(i)
     current_file_path = os.path.join(path, current_file_name)
+
     if not os.path.isfile(current_file_path):
         continue
 
     if current_file_name.lower() == "file_name_changer.exe":
         continue
 
-    current_file_type = ""
-    if "." in current_file_name:
-        current_file_type = str(current_file_name).split(".").pop()
+    current_file_type = get_file_type(current_file_name)
 
-    target_file_name = get_modified_date(current_file_path)
+    target_file_name = get_time_of_creation(current_file_path)
     target_file_path = os.path.join(path, target_file_name)
 
     print(f"{current_file_name} \t--> {append_file_type(target_file_name, current_file_type)}")
